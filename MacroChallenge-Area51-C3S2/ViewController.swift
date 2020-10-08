42b47f103ef54591b9abaf70e212b717606efc01
alnodiadnan <55888669+alnodiadnan@users.noreply.github.com>
2020-10-08 17:12:52 +0700

    Initial Commit

---
+++ MacroChallenge-Area51-C3S2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacroChallenge-Area51-C3S2
+//
+//  Created by Alnodi Adnan on 08/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MacroChallenge-Area51-C3S2Tests/MacroChallenge_Area51_C3S2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MacroChallenge_Area51_C3S2Tests.swift
+//  MacroChallenge-Area51-C3S2Tests
+//
+//  Created by Alnodi Adnan on 08/10/20.
+//
+
+import XCTest
+@testable import MacroChallenge_Area51_C3S2
+
+class MacroChallenge_Area51_C3S2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
